9b2a0186345ce0c243fa6ddfd03dcc69e920c7ad
Zach Martin <vzachmartin@gmail.com>
2021-01-23 10:25:17 -0900

    Remove Roles from functions

--- databaseSetup/setup/functions.js
+++ databaseSetup/setup/functions.js
@@ -6,36 +6,31 @@ import { RefreshToken } from '../queries/auth-refresh.js'
 
 import faunadb from 'faunadb'
 const q = faunadb.query
-const { Query, Lambda, Role, Var } = q
+const { Query, Lambda, Var } = q
 
 const RegisterUDF = CreateOrUpdateFunction({
   name: 'register',
   body: Query(Lambda(['email', 'password'], RegisterAccount(Var('email'), Var('password')))),
-  role: Role('functionrole_register')
 })
 
 const LoginUDF = CreateOrUpdateFunction({
   name: 'login',
   body: Query(Lambda(['email', 'password'], LoginAccount(Var('email'), Var('password')))),
-  role: Role('functionrole_login')
 })
 
 const RefreshTokenUDF = CreateOrUpdateFunction({
   name: 'refresh_token',
   body: Query(Lambda([], RefreshToken())),
-  role: Role('functionrole_refresh_tokens_logout')
 })
 
 const LogoutAllUDF = CreateOrUpdateFunction({
   name: 'logout_all',
   body: Query(Lambda([], LogoutAllSessions())),
-  role: Role('functionrole_refresh_tokens_logout')
 })
 
 const LogoutUDF = CreateOrUpdateFunction({
   name: 'logout',
   body: Query(Lambda([], LogoutCurrentSession())),
-  role: Role('functionrole_refresh_tokens_logout')
 })
 
 export { RegisterUDF, LoginUDF, RefreshTokenUDF, LogoutAllUDF, LogoutUDF }
