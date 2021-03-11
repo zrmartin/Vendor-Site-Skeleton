fd3d4a59ac2fdf10d2df91b0a422d39191485e01
Zach Martin <vzachmartin@gmail.com>
2021-03-10 17:04:15 -0900

    Unathenticated page shows Login Modal

--- pages/unauthenticated.js
+++ pages/unauthenticated.js
@@ -1,11 +1,18 @@
-import { Login } from '../components'
+import { LoginRegisterModal } from '../components'
+import { useRouter } from 'next/router'
+
 const UnauthenticatedPage = ({ message, showLogin }) => {
+  const router = useRouter()
   return (
-      <>
+    <>
+      {showLogin ? (
+        <LoginRegisterModal onClose={() => {router.back()}} isOpen={true} message={message}/>
+      ) : (
         <h3>{message}</h3>
-        {showLogin && <Login/>}
-      </>
-  );
-};
+      )
+      }
+    </>
+  )
+}
 
 export default UnauthenticatedPage
